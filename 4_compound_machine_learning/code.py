7f437a9815658cf45f808cd74eb0495bb45879c0
locklearasd <92257555+locklearasd@users.noreply.github.com>
2026-04-08 01:01:43 +0900

    4_compound_machine_learning has finished

--- 4_compound_machine_learning/code.py
+++ 4_compound_machine_learning/code.py
@@ -2,34 +2,102 @@ from typing import List, Union
 import numpy.typing as npt
 import numpy as np
 import pandas as pd
+from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
+from rdkit import Chem
+from rdkit.Chem import Descriptors, Draw
+from sklearn.model_selection import GridSearchCV
 
 def draw_molecule(csvfile: str) -> None:
     # 課題 4-1
+    csv = pd.read_csv(csvfile)
+    row  = csv[csv["Compound ID"] == "CHEMBL540227"]
+    smile = row["SMILES"].iat[0]
+    mol = Chem.MolFromSmiles(smile)
+    Draw.MolToFile(mol, "data/CHEMBL540227.png")
     pass
 
-def create_2d_descriptors(smiles: str) -> Union[npt.NDArray[np.float_], List[float]]:
+def create_2d_descriptors(smiles: str) -> Union[npt.NDArray[np.float64], List[float]]:
     # 課題 4-2
-    return []
+    mol = Chem.MolFromSmiles(smiles)
+    if mol is None:
+        return []
 
-def predict_logpapp(csvfile: str) -> Union[npt.NDArray[np.float_], pd.Series, List[float]]:
+    desc_values = []
+    for _, desc_func in Descriptors._descList:
+        desc_values.append(desc_func(mol))
+    return desc_values
+
+
+
+def predict_logpapp(csvfile: str) -> Union[npt.NDArray[np.float64], pd.Series, List[float]]:
     # 課題 4-3
     np.random.seed(0) # 出力を固定するためにseedを指定
     rfr = RandomForestRegressor(random_state=0) # 出力を固定するためにrandom_stateを指定
 
-    return 0.0
+    x = []
+    y = []
+    csv = pd.read_csv(csvfile)
+    for _, row in csv.iterrows():
+        smiles = str(row["SMILES"]).strip()
+        desc_values = create_2d_descriptors(smiles)
+        if len(desc_values) == 0:
+            continue
+        x.append(desc_values)
+        y.append(row["LogP app"])
+
+    X = np.array(x, dtype=np.float64)
+    Y = np.array(y, dtype=np.float64)
+    # Xは説明変数、yは目的変数
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, train_size=700, random_state=0)
+    rfr.fit(X_train, y_train)
+    Y_pred = rfr.predict(X_test)
+
+    return Y_pred
 
 def grid_search(csvfile: str) -> float:
     # 課題 4-4
     # こちらも出力を固定するためにseedやrandom_stateを指定すること
     np.random.seed(0)
 
-    # # Xは説明変数、yは目的変数
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=700, random_state=0)
-    # 
+    x = []
+    y = []
+    csv = pd.read_csv(csvfile)
+    for _, row in csv.iterrows():
+        smiles = str(row["SMILES"]).strip()
+        desc_values = create_2d_descriptors(smiles)
+        if len(desc_values) == 0:
+            continue
+        x.append(desc_values)
+        y.append(row["LogP app"])
 
-    return 0.0
+    X = np.array(x, dtype=np.float64)
+    Y = np.array(y, dtype=np.float64)
+    # Xは説明変数、yは目的変数
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, train_size=700, random_state=0)
+
+    rfr = RandomForestRegressor(random_state=0)  # 出力を固定するためにrandom_stateを指定
+    param_grid = {
+        "n_estimators": [100, 200, 400],
+        "max_depth": [5, 10, 15],
+    }
+
+    gscv = GridSearchCV(
+        estimator=rfr,
+        param_grid=param_grid,
+        scoring="neg_mean_squared_error",
+        n_jobs=-1,
+        cv=4,
+    )
+
+    gscv.fit(X_train, y_train)
+
+    best_model = gscv.best_estimator_
+    Y_pred = best_model.predict(X_test)
+    RMSE = mean_squared_error(y_test, Y_pred, squared=False)
+
+    return RMSE
 
 if __name__ == "__main__":
     smiles = "C(=O)(c1ccc(OCCCCCC)cc1)CCNc1cc(Cl)ccc1"
